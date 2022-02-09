eac62db8f001def76bf56a5504b8d5864cb48ea6
alejaromerom <aleromero43@gmail.com>
2022-02-09 10:28:20 -0500

    Javascript- tipos de datos

--- Leccion02/Tiposdedatos.js
+++ Leccion02/Tiposdedatos.js
@@ -5,7 +5,35 @@ Ejemplos de tipos de datos JavaScript
 //tipo de dato string
 
 var nombre = "Alejandra" 
-console.log(nombre);
-var numero= 1000; 
+console.log(typeof nombre);
 
+nombre = 10;
+
+console.log(typeof nombre);
+
+//tipo de dato númerico 
+var numero= 1000; 
 console.log(numero); 1000 
+
+//tipo de dato object
+
+var object= {
+
+nombre : "Alejandra", 
+apellido: "Romero",
+telefono: "8125428"
+}
+console.log(typeof object);  
+
+//tipo de dato boolean (true, false)
+
+var bandera= true;  
+console.log (bandera); 
+
+//tipo de dato funcion 
+
+function myfuction () {
+    
+}
+
+console.log(myfuction); 
