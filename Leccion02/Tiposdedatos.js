53ba0f561b5d0cc5ee083f9c6e5f7c0193d66eea
alejaromerom <aleromero43@gmail.com>
2022-02-14 11:29:24 -0500

    Arrays-strings

--- Leccion02/Tiposdedatos.js
+++ Leccion02/Tiposdedatos.js
@@ -71,3 +71,14 @@ var y =null;
 
 console.log(typeof y);
 
+console.log(typeof y);
+
+var autos = ['BMW','AUDI', 'MAZDA'];
+
+console.log(autos);
+console.log(typeof autos)
+
+var  z = ''; 
+
+console.log(z)
+console.log(typeof z);
