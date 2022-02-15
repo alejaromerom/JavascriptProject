e6141bfdac7f248bbc699da8d19e46c5089c2316
alejaromerom <aleromero43@gmail.com>
2022-02-15 09:37:28 -0500

    Observaciones del codigo

---
+++ Leccion02/Buenas-practicas-js.js
@@ -0,0 +1,44 @@
+
+// La primera letra debe estar con mayuscula en la siguiente palabra
+let nombreCompleto1 = " Alejandra Romero";
+//Const: objeto que ya existe en Js
+//Log: Función-metodo por eso utilizamos parantesis pasamos la info que neceista la fucion para trabajar en este caso se le conoce como argumento
+// pasamos el valor de nombreCompleto que es el argumento que necesita esta función para trabajar
+console.log(nombreCompleto1);
+
+
+// x y Y  almacenan valores numericos 
+let x, y; 
+
+x=10, y =20; 
+
+let z = x + y ; 
+
+console.log(z); 
+// Js es sensible a Mayus y minus (solo son permitidas letras en minus o mayus pero no combinadas con numeros)
+// tambien es valido utilizar caracteres excepto los especiales
+let nombreCompleto = "Sebastian Martinez";
+let nombrecompleto = "Alejandra Romero";
+
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+//otros ejemplos de variables permitidas en js 
+
+let a1nombrecompleto; 
+let _nombreCompleto;
+let $nombreCompleto; 
+
+// 1nombreCompleto; no está permitido iniciar el nombre de una variable con numeros 
+
+let _break = 10; 
+let break1 = 10; 
+//no es recomendable utilizar nombres con palabras reservadas 
+
+//otro ejemplo 
+
+let ruptura = 10; 
+
+
+
+
