814dd54a8f623254f9dc93e7d1c9796038bc8d1c
alejaromerom <aleromero43@gmail.com>
2022-02-15 08:42:05 -0500

    Solucion-tipos de datos

---
+++ Leccion02/concatenacion.js
@@ -0,0 +1,17 @@
+var nombre = 'juan';
+var apellido = 'perez'; 
+
+var nombreCompleto = nombre + ''+ apellido; 
+console.log(nombreCompleto);
+
+var nombreCompleto2 ='Sebastian' + ' ' + 'Martinez ';
+console.log(nombreCompleto2); 
+
+var x = nombre + 2 + 4 ;
+console.log(x);
+
+x = nombre  + (2 + 4);
+console.log(x); 
+
+x = 2 + 4 + nombre; 
+console.log(x)
