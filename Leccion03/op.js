1755f3d2bc474f6201bde2e67b46f3fefd579f3c
alejaromerom <aleromero43@gmail.com>
2022-02-16 10:41:36 -0500

    Presedecnia de operaciones Js

--- Leccion03/op.js
+++ Leccion03/op.js
@@ -1,12 +1,15 @@
-let a =3,b = 2, c=1;
+let a =3,b = 2, c=1,d=4;
 
 //Se evalua de izquierda a derecha
-let z = a*b + c ;
+let z = a*b + c / d;
 console.log(z);
 
 //Toma mayor prioridad la multiplicacion 
-
-z= c + a* b;
+// De izquierda a derecha y al final la suma
+z= c + a* b /d;
 
 console.log(z)
 
+z= (c+a)* b/c; 
+
+console.log(z);
