369dec24f71463f753268b5109f9b2c857e7b82e
alejaromerom <aleromero43@gmail.com>
2022-02-16 10:48:30 -0500

    Ejemplo resuelto

---
+++ Leccion03/designacion.js
@@ -0,0 +1,18 @@
+//El operador asigna el valor a la variable que estamos creando o sea a
+
+let a = 1; 
+
+a += 3; // a=a + 3, en una sola expresion se incrementa
+console.log(a); 
+
+a -= 2; // a=a -2
+console.log(a)
+
+/*Otros operadores 
+
+*=
+/=
+%=
+**=
+
+*/ 
