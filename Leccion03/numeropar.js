1557bae5c69f6983e20a77b8b78ce58302c89d58
alejaromerom <aleromero43@gmail.com>
2022-02-16 15:16:40 -0500

    relaciones

---
+++ Leccion03/numeropar.js
@@ -0,0 +1,14 @@
+let a = 9; 
+
+if (a % 2 ==0) {
+
+    //  La condición nos representa dependiendo del valor que le pongamos a la variable se ejecutará siendo par o impar
+
+    console.log("Es un numero par");
+
+}
+
+else {
+console.log("Es numero impar");
+
+}
