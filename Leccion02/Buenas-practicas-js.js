9ad915136fe0181513a4921dba4230744266af8e
alejaromerom <aleromero43@gmail.com>
2022-02-16 10:31:12 -0500

    operadores-js

--- Leccion02/Buenas-practicas-js.js
+++ Leccion02/Buenas-practicas-js.js
@@ -42,3 +42,5 @@ let ruptura = 10;
 
 
 
+
+
---
+++ Leccion03/op.js
@@ -0,0 +1,12 @@
+let a =3,b = 2, c=1;
+
+//Se evalua de izquierda a derecha
+let z = a*b + c ;
+console.log(z);
+
+//Toma mayor prioridad la multiplicacion 
+
+z= c + a* b;
+
+console.log(z)
+
