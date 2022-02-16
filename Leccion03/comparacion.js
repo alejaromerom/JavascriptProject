18b7b91a6561385215fcd4f40319fba1a842271c
alejaromerom <aleromero43@gmail.com>
2022-02-16 10:59:47 -0500

    operaciones js

---
+++ Leccion03/comparacion.js
@@ -0,0 +1,9 @@
+//comparacion ==
+// el valor cambia de true a false dependiendo del valor que asignemos
+let a=3, b =2, c="3"; 
+let z = a== c; // se revisa el valor sin importar el tipo: ===
+console.log(z); 
+
+
+z= a === c;//revisa si los valores son iguales pero tambien los tipos 
+console.log(z);
