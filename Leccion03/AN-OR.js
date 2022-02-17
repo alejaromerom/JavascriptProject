3697a3131661e7b55131ad07f7d51dfa97222224
alejaromerom <aleromero43@gmail.com>
2022-02-17 18:08:50 -0500

    Numeros-pares.impares-ejercicio

---
+++ Leccion03/AN-OR.js
@@ -0,0 +1,18 @@
+let a=5; 
+//valor minimo 0
+let ValMin = 0, valMax=10; 
+if(a>=ValMin && a <= valMax){
+// Se pregunta si la varible a es mayor o igual que el valor minimo 
+// si no se cumple mostrará que está fuera del rango
+    console.log("Dentro del rango");
+}
+
+    
+
+else {
+
+    console.log("fuera del rango");
+
+}
+
+    
---
+++ Leccion03/edad.js
@@ -0,0 +1,11 @@
+let edad = 17, adulto =18
+// El programa imprime por consola las variables que les designemos esa mayot o menor
+if (edad >=18) {
+
+console.log("es un adulto")
+
+}
+else{
+
+    console.log("es menor de edad"); 
+}
